c764d0b0f6bacd37a1a8cb0a8b29b01c751d2cb7
ogonzalez-webimpacto <ogonzalez@webimpacto.es>
2023-01-20 10:13:46 -0600

    Update index.ts

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -1,5 +1,6 @@
 import { IOClients } from '@vtex/api'
-
+import { masterDataFor } from '@vtex/clients'
+import { myEntity } from '../../masterdata/myEntity/schema' // Insert here the names of your data entity, store and app.
 import Status from './status'
 
 // Extend the default IOClients implementation with our own custom clients.
@@ -7,4 +8,9 @@ export class Clients extends IOClients {
   public get status() {
     return this.getOrSet('status', Status)
   }
+}
+
+
+public get entity() {
+  return this.getOrSet('entity', masterDataFor<myEntity>('entity'))
 }
