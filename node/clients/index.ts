424bc456a32247a19a75be856e5bb014db70fed8
ogonzalez-webimpacto <ogonzalez@webimpacto.es>
2023-01-25 10:28:51 -0600

    add trigger

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -1,6 +1,6 @@
 import { IOClients } from '@vtex/api'
 import { masterDataFor } from '@vtex/clients'
-import { myEntity } from 'webimpacto.work-with-us-service' // Insert here the names of your data entity, store and app.
+import { candidatos } from 'webimpacto.work-with-us-service' // Insert here the names of your data entity, store and app.
 import Status from './status'
 
 // Extend the default IOClients implementation with our own custom clients.
@@ -14,6 +14,6 @@ export class Clients extends IOClients {
 public get entity() {
   return this.getOrSet(
     'entity', 
-    masterDataFor<myEntity>('candidatos')
+    masterDataFor<candidatos>('candidatos')
     )
 }
